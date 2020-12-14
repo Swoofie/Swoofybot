590b1453be0e077c3007c178262cae23d54238aa
Swoofie <64838366+Swoofie@users.noreply.github.com>
2020-12-14 15:23:46 +0000

    Update 0.01
    Set Prefix to !
    Added !truth command to test code

---
+++ commands/truth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'truth',
+    description: "The bot speaks only the truth",
+    execute(message, args){
+        message.channel.send('Swoofy is wholesome');
+    }
+}
--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const Discord = require('discord.js');
-const config = require('/config.json');
+const config = require('./config.json');
 const client = new Discord.Client();
+
+const prefix = '!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 
 client.once('ready', () => {
     console.log('Swoofy Bot is Online!');
 });
 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot)return;
 
 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'truth'){
+        client.commands.get('truth').execute(message, args);
+    }
+})
 
 
 
