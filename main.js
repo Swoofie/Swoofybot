87f8dce3b58a487ce888a5d43acd1d204fe2fcb3
Swoofie <64838366+Swoofie@users.noreply.github.com>
2020-12-14 14:20:30 +0000

    Linked to discord bot
    Bot now linked to discord!

---
+++ main.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Swoofy Bot is Online!');
+});
+
+
+
+
+
+
+
+
+
+
+
+//Keep login at end of document
+client.login('Nzg4MDQ0ODYzNzA1ODQxNjk1.X9dx-A.w5Kn1VFDKJmLyQdEgR9n8n5_pu0');
+
