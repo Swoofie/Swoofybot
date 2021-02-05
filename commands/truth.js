6dd2150d454f4a557489b438dde76ab10a307541
Swoofie <64838366+Swoofie@users.noreply.github.com>
2021-02-05 11:33:17 +0000

    Added new user to truth
    Added New user to truth as i begin to work this bot into 2 new servers.

--- commands/truth.js
+++ commands/truth.js
@@ -7,6 +7,7 @@ module.exports = {
         const userID2 ="274918906718584833";
         const userID = "373422145386053633";
         const userID3 = "368192371353059341";
+        const userID4 = "119088036678991873";
 
      if(message.author.id === userID) {
         message.channel.send('You are wholesome')
@@ -15,6 +16,8 @@ module.exports = {
         message.channel.send('You are also wholesome')
     } else if(message.author.id === userID3){
         message.channel.send('Yes you wholesome lil cow <3')
+    } else if(message.author.id === userID4){
+        message.channel.send('We will go to the sky master! Please keep up your amazing map work as we grow together and I go on even more adventures!~')
     } else
         message.channel.send('You are lewd').catch(console.error);
     }
