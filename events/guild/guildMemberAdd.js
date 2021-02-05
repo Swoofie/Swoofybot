c8dc93742cd60ec15e22cf99d82fdb88676b7d66
Swoofie <64838366+Swoofie@users.noreply.github.com>
2021-02-05 15:19:35 +0000

    Improved GuildMemberAdd
    GuildMemberAdd has been moved over to new cleaner system

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,8 +1,7 @@
-module.exports = (Discord, client, message, args, config) => {
-    client.on('guildMemberAdd', guildMember =>{
-        let welcomeRole =guildMember.guild.roles.cache.find(role => role.name === 'Members');
-    
-        guildMember.roles.add(welcomeRole);
-        guildMember.guild.channels.cache.get('807248377607356457').permissionsLocked(`<@${guildMember.user.id}> welcome to the server!`)
-    });
+module.exports = async (Discord, client, member) => 
+{
+  const guild = member.guild
+    let welcomeRole = guild.roles.cache.find(role => role.name === 'Comfy Cats');
+    member.roles.add(welcomeRole);
+    guild.channels.cache.get('807248377607356457').send(`<@${member.user.id}> welcome to the server!`)
 }
